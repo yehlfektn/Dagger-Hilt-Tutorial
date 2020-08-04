e122c02fd6299f206ed15318632b56e44df6e0df
nurdauletkenges <nurdaulet.kenges@nu.edu.kz>
2020-08-04 13:24:37 +0600

    -- Removing wrong library

--- app/src/main/java/com/mindorks/framework/mvvm/App.kt
+++ app/src/main/java/com/mindorks/framework/mvvm/App.kt
@@ -1,7 +1,6 @@
 package com.mindorks.framework.mvvm
 
 import android.app.Application
-import com.github.ajalt.timberkt.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
