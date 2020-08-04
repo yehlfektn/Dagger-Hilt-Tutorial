2d68d20e2698be0a550f6a2bed38f953d272bd88
nurdauletkenges <nurdaulet.kenges@nu.edu.kz>
2020-08-04 13:17:37 +0600

    -- Adding Timber library for logging. -- Planting Tree in the Application class.

--- app/src/main/java/com/mindorks/framework/mvvm/App.kt
+++ app/src/main/java/com/mindorks/framework/mvvm/App.kt
@@ -1,7 +1,16 @@
 package com.mindorks.framework.mvvm
 
 import android.app.Application
+import com.github.ajalt.timberkt.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
